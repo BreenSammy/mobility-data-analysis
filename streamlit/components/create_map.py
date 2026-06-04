4704a4a95f1b08e3ddd68f675b23b54cdb17c813
Sammy Breen <s.breen@tum.de>
2026-06-04 12:52:00 +0200

    Added stadia maps tiles

--- streamlit/components/create_map.py
+++ streamlit/components/create_map.py
@@ -1,6 +1,13 @@
 import folium
 import streamlit as st
 
+attr = (
+    '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> '
+    '&copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> '
+    '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
+)
+tiles = f"https://tiles.stadiamaps.com/tiles/alidade_smooth/{{z}}/{{x}}/{{y}}{{r}}.png?api_key={st.secrets['STADIA_KEY']}"
+
 
 def create_map(center_start: list[float], zoom_start: int) -> folium.Map:
     if "center" not in st.session_state:
@@ -9,4 +16,6 @@ def create_map(center_start: list[float], zoom_start: int) -> folium.Map:
     if "zoom" not in st.session_state:
         st.session_state["zoom"] = zoom_start
 
-    return folium.Map(location=center_start, zoom_start=zoom_start)
+    return folium.Map(
+        location=center_start, zoom_start=zoom_start, attr=attr, tiles=tiles
+    )
